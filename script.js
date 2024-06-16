55c9c7a08459b34061bfac635a3da6946b83c677
Abhi70815 <abhishekchaurasia45621@gmail.com>
2024-06-16 22:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++) {
+		let isrepeated=false;
+		for(let j=0;j<str.length;j++){
+        if(i!==j && str[i]===str[j]){
+        isrepeated=true;
+			break;
+		}
+		}
+		if(!isrepeated){
+    return str[i];
+		}
+		
+		
+}
+	return  null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
